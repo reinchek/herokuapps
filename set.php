3ac96acd6e89bbe55609851240ed957f52de0458
Nino Marrazzo <ni.marrazzo@gmail.com>
2017-03-17 15:18:58 +0100

    Test set.php

--- set.php
+++ set.php
@@ -1,15 +1,15 @@
 <?php
 
 	// Load composer
- 	require __DIR__ . '/vendor/autoload.php';
-
+//require __DIR__ . '/vendor/autoload.php';
+//
 	$API_KEY  = '';
-	$BOT_NAME = '';
-	$hook_url = 'https://reinbot.herokuapp.com';
-	try {
-		$telegram = new Longman\TelegramBot\Telegram($API_KEY, $BOT_NAME);
-
-	}
+//$BOT_NAME = '';
+//$hook_url = 'https://reinbot.herokuapp.com';
+//try {
+//	$telegram = new Longman\TelegramBot\Telegram($API_KEY, $BOT_NAME);
+//
+//}
 
 	echo 'lota';
 
